a9ac5dcdf78f44cd9cd641a53deb5d4cdb0179e5
Viraj Sonaje <virajsonaje@gmail.com>
2024-03-13 20:56:40 -0700

    game decluterize and async functions (#8)
    * game decluterize and async functions
    
    * Play Smooth operator

--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,8 @@ import Dice from './Dice'
 import Board from './Board';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect, createContext, useRef } from 'react';
-import { gsap } from "gsap";
+import gsap from "gsap";
+import {useGSAP} from '@gsap/react';
 
 
 import Row from 'react-bootstrap/Row';
@@ -13,70 +14,72 @@ export const playerContext = createContext();
 export const ladderContext = createContext();
 export const refContext = createContext();
 
+const ladderMap = new Map([
+  [10,12],
+  [11,33],
+  [20,38],
+  [6,24],
+  [40,59],
+  [45,54],
+  [64,78],
+  [72,91],
+  [86,96]
+
+]);
+const snakeMap = new Map([
+  [19, 4],
+  [13, 7],
+  [48, 14],
+  [57, 36],
+  [68, 49],
+  [83, 61],
+  [87, 66],
+  [94, 88],
+  [98, 84]
+]);
 
 function Game(){
-  const nodeRef = useRef(null);
+    const nodeRef = useRef(null);
     const [diceNum, setDiceNum] = useState(6);
     const [player, setPlayer] = useState(1);
     const [reachedItemFlag, setReachedItemFlag] = useState(false);
-    const ladderMap = new Map([
-      [10,12],
-      [11,33],
-      [20,38],
-      [6,24],
-      [40,59],
-      [45,54],
-      [64,78],
-      [72,91],
-      [86,96]
-
-  ]);
-  const snakeMap = new Map([
-    [19, 4],
-    [13, 7],
-    [48, 14],
-    [57, 36],
-    [68, 49],
-    [83, 61],
-    [87, 66],
-    [94, 88],
-    [98, 84]
-
-]);
+    const animStartObj = useRef(null);
   
   let snlobj = {
     ladders: ladderMap,
     snakes: snakeMap
   };
+
+// let useflag = 0;
+async function roll(){
+      let [diceRoll, nextPlayerPos] = await movePlayer(player);
+      
+      animStartObj.current = await getPlayerCoords(nodeRef.current);
+
+      setDiceNum(diceRoll);
+      setPlayer(nextPlayerPos);
   
-  let prevPersonObj;
-  let personStart;
-    function roll(){
-        let min = 1;
-        let max = 6;
-        let randDice = Math.floor(Math.random() * (max - min +1) ) + min;
-        let nextPlayerPos = randDice + player;
-        prevPersonObj = nodeRef.current;
-        //gsap.to(nodeRef.current, {color:"red", y:100, duration:2});
-        //gsap.to("#personAnim", {color:"green", y:100, duration:1});
-        //personStart = prevPersonObj.getBoundingClientRect();
-        console.log(personStart);
-        if(nextPlayerPos > 100) nextPlayerPos = nextPlayerPos - randDice;
-        setDiceNum(randDice);
-        //timeout
-        setPlayer(nextPlayerPos);
-        
-        if(ladderMap.has(nextPlayerPos) || snakeMap.has(nextPlayerPos))
-          setReachedItemFlag(true);
-        // checkBonus();
+      setTimeout(() => {
+        if(ladderMap.has(nextPlayerPos) || snakeMap.has(nextPlayerPos)){
+          animStartObj.current = getPlayerCoords(nodeRef.current);
+          setTimeout(() => {  
+              setReachedItemFlag(true);
+          }, 500);
+        }
+      }, 500);
+      
+
+      // checkBonus();
     }
 
-    // useEffect(()=>{
-     
-    //     gsap.from(nodeRef.current, {x:personStart.left, y: personStart.top});
-    //     console.log(personStart);
-      
-    // }, [personStart]);
+    useGSAP(()=>{
+      if(animStartObj.current)  {
+        console.log(animStartObj.current);
+        const {top, left} = nodeRef.current.getBoundingClientRect();
+        gsap.from(nodeRef.current, {x: animStartObj.current.left-left, y: animStartObj.current.top-top, 
+        duration: 0.5});
+      } 
+    },{ dependencies: [player], revertOnUpdate: false});
 
 
     function resetGame(){
@@ -112,3 +115,19 @@ function Game(){
   }
   
   export default Game;
+
+function getPlayerCoords(domObj){
+  const {top, left} = domObj.getBoundingClientRect();
+  return {top, left};
+}
+
+function movePlayer(player){
+  let min = 1;
+  let max = 6;
+  let randDice = Math.floor(Math.random() * (max - min +1) ) + min;
+  let nextPlayerPos = randDice + player;
+
+  if(nextPlayerPos > 100) nextPlayerPos = nextPlayerPos - randDice;
+
+  return [randDice, nextPlayerPos];
+}
--- src/Person.js
+++ src/Person.js
@@ -2,13 +2,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPerson } from '@fortawesome/free-solid-svg-icons'
 import { useState, useEffect, createContext, useContext, useRef } from 'react';
 import {refContext} from './Game';
-import { gsap } from "gsap";
+
 
 function Person({pos, position}){
   // gsap.to("#personAnim", {color:"green", y:100, duration:1});
   const ref = useContext(refContext);
     if(pos === position){
-      console.log(position);
       return  (
       <>
       {/* <FontAwesomeIcon className="personIcon" icon={faPerson}/> */}
